d7a228e757b97d694bc228fe13f277a5ed3c6a7b
CnineWidom <2779386157@qq.com>
2019-03-25 15:41:14 +0800

    上传和下载图片代码测试类

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('test','loginController@index')->name('test');
+
+Route::post('dologin','loginController@dologin');
+Route::get('dologin','loginController@dologin')->name('dologin');
+
+Route::get('upload/doupload','uploadController@doupload')->name('doupload');
+Route::post('upload/doupload','uploadController@doupload');
+
+Route::post('upload/download','uploadController@download');
+//Route::group(['prefix'=>'upload','middleware'=>'doupload'],function (){
+//    Route::get('/doupload','loginController@dologin');
+//});
